243ab6cb47f5b70bd4d124cd06919c897212991b
Martin Kopec <mkopec@redhat.com>
2018-05-28 09:30:07 +0000

    Generate accounts.yaml
    Generate accounts.yaml file when create_accounts_file
    is specified and test_accounts is not.
    
    Story: 2001693
    Task: 8685
    
    Change-Id: I7ed8390b29a430d97e1846f8f868f5b6af8eb02b

---
+++ config_tempest/tests/test_accounts.py
@@ -0,0 +1,47 @@
+# Copyright 2018 Red Hat, Inc.
+# All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import mock
+import os
+
+from config_tempest import accounts
+from config_tempest.tests.base import BaseConfigTempestTest
+
+
+class TestAccounts(BaseConfigTempestTest):
+    """Accounts test class
+
+    Tests for create_accounts_file and write_accounts_file methods.
+    """
+
+    def setUp(self):
+        super(TestAccounts, self).setUp()
+        self.conf = self._get_conf("v2.0", "v3")
+
+    @mock.patch('config_tempest.accounts.write_accounts_file')
+    def test_create_accounts_file(self, mock_write):
+        path = "./etc/accounts.yaml"
+        # credentials under auth section
+        accounts.create_accounts_file(True, path, self.conf)
+        mock_write.assert_called_with(path, "admin", "adminPass",
+                                      "adminTenant")
+        self.assertEqual(self.conf.get("auth", "test_accounts_file"),
+                         os.path.abspath(path))
+
+        # credentials under identity section
+        accounts.create_accounts_file(False, path, self.conf)
+        mock_write.assert_called_with(path, "demo", "secret", "demo")
+        self.assertEqual(self.conf.get("auth", "test_accounts_file"),
+                         os.path.abspath(path))
