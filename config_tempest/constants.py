78317425bb0d0a3a5768a5a1225cacead51d2a4d
Chandan Kumar (raukadah) <chkumar@redhat.com>
2019-05-29 19:40:29 +0530

    Update default cirros image to 0.4.0
    Since older cirros images has issues, we need to switch to
    cirros 0.4.0 image as devstack uses the same.
    
    Change-Id: I4bb6bae895ba8f8432fd0985e8b8f217adcf92f3
    Signed-off-by: Chandan Kumar (raukadah) <chkumar@redhat.com>

--- config_tempest/constants.py
+++ config_tempest/constants.py
@@ -23,8 +23,8 @@ TEMPEST_WORKSPACE = os.getcwd()
 
 DEPLOYER_INPUT = os.path.join(os.path.expanduser("~"),
                               "tempest-deployer-input.conf")
-DEFAULT_IMAGE = ("http://download.cirros-cloud.net/0.3.5/"
-                 "cirros-0.3.5-x86_64-disk.img")
+DEFAULT_IMAGE = ("http://download.cirros-cloud.net/0.4.0/"
+                 "cirros-0.4.0-x86_64-disk.img")
 DEFAULT_IMAGE_FORMAT = 'qcow2'
 
 DEFAULT_FLAVOR_RAM = 64
